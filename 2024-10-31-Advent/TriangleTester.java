a19a96da55c623c03a7c1a38595b27595866c8c3
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-11-05 20:26:32 -0500

    fixed some of the CountTrianglesA still doesnt work

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -4,27 +4,33 @@ import java.util.Scanner;
 
 public class TriangleTester{
   public static void main(String[] args){
-    System.out.println(isTrig(new int[] {1,11,1312}));
-    System.out.println(isTrig(new int[] {5,11,13}));
+    System.out.println(countTrianglesA("inputTri.txt")); 
   }
 
   public static int countTrianglesA(String filename){
     int[] sides = new int[3];
+    int count = 0;
+    try {
     File fille = new File(filename);
     Scanner file = new Scanner(fille);
-    int count = 0;
     while(file.hasNextLine()){
       int index = 0;
-      while(file.hasNextInt()){
+      while(file.hasNextInt() && index < 3){
         sides[index] = file.nextInt();
         index ++;
         }
-      if(isTrig(sides)){
+      if(index == 3 && isTrig(sides)){
         count ++;
       }
       sides = new int[3];
-    }
   }
+  file.close();
+  } catch (FileNotFoundException e) {
+  System.out.println("File not found: " + filename);
+  }
+  return count;
+  }
+
 
   public static boolean isTrig (int[] sides){
     int max = 0;

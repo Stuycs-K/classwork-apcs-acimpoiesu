30fb59ce6178e48d6cb8f3ede3dfcdd121db275d
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-11-07 17:48:24 -0500

    added speaks for animal+bird combinations

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -2,8 +2,8 @@
 // but when we called the Bird we would get the newly updated getName() instead
 // of the original name since we overrid it.
 // A animal being made with bird-like properties doesn't work because
-// not all animals have bird-like properties and so you shouldn't make
-// an animal with bird-like properties. It doesn't work because it doesn't have some of the properties like height or color
+// not all animals have bird-like properties or "is-A" bird and so you shouldn't make
+// all animals have bird-like properties. It doesn't work because it doesn't have some of the properties like height or color
 public class Driver{
   public static void main(String[]args){
     Animal Jimmeth = new Animal ("ahhhh", 9, "Jimmeth");
@@ -16,5 +16,8 @@ public class Driver{
     Bird b1 = new Bird("tweet", 3, "Chirpy", 10.5, "blue");
     //Bird b2 = new Animal("owww", 4, "Human");
     Animal a2 = new Bird("meow", 2, "notabird", 12.0, "blue");
+    a1.speak();
+    b1.speak();
+    a2.speak();
   }
 }

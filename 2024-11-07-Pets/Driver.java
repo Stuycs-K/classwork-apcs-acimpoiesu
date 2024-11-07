97005e84343a03d84f28055d2ae44455de89bf7a
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-11-07 11:47:03 -0500

    added response at top of Driver that explains the changes

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,6 @@
+//When we called the getName for the animal we were given the original name given
+// but when we called the Bird we would get the newly updated getName() instead
+// of the original name since we overrid it.
 public class Driver{
   public static void main(String[]args){
     Animal Jimmeth = new Animal ("ahhhh", 9, "Jimmeth");

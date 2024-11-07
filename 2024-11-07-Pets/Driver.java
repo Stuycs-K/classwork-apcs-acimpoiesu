fd01bb6c76a4a6844e3a0113f281a55949659c8b
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-11-07 11:43:38 -0500

    overrided getName with working test cases

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,9 +1,10 @@
 public class Driver{
   public static void main(String[]args){
-    /*Animal Jimmeth = new Animal ("ahhhh", 9, "Jimmeth");
-    Jimmeth.speak();
+    Animal Jimmeth = new Animal ("ahhhh", 9, "Jimmeth");
+    //Jimmeth.speak();
     Bird jimmeth2 = new Bird("arg", 7, "static",1515151.64, "yellow");
-    jimmeth2.speak();
-    */
+    //jimmeth2.speak();
+    System.out.println(Jimmeth.getName());
+    System.out.println(jimmeth2.getName());
   }
 }

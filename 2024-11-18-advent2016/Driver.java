5d480abd70a884db479ae0e7468b27b9ccb62d2b
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-11-18 11:46:04 -0500

    added Driver and Santa.java

---
+++ 2024-11-18-advent2016/Driver.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Driver{
+  public static int Distance(String filename){
+    try {
+        File file = new File(filename);
+        Scanner input = new Scanner(file);
+        while (input.hasNextString()){
+          if input.nextString().charAt
+        }
+  }
+}

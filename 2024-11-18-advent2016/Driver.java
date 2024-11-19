7c509d53ba843c25109c26a262d1a276759f25e7
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-11-19 11:42:14 -0500

    day 1 still not working

--- 2024-11-18-advent2016/Driver.java
+++ 2024-11-18-advent2016/Driver.java
@@ -5,10 +5,30 @@ import java.util.Scanner;
 public class Driver{
   public static int Distance(String filename){
     try {
+        int dir = 0;
+        int [] blocks = new int [4];
         File file = new File(filename);
         Scanner input = new Scanner(file);
-        while (input.hasNextString()){
-          if input.nextString().charAt
+        String line = input.nextLine();
+        String [] dirs = line.split(", ");
+        for (int i = 0; i<dirs.length; i++){
+          if (dirs[i].substring(0,1).equals("R")){
+            dir++;
+            blocks[dir] += Integer.parseInt(dirs[i].substring(1));
+          }
+          else{
+            dir--;
+            blocks[dir] += Integer.parseInt(dirs[i].substring(1));
+          }
         }
-  }
+        System.out.println(blocks.toString());
+        return blocks[0] + blocks[1]+blocks[2]+blocks[3];
+      }
+      catch(Exception e){
+      return 0;
+      }
+    }
+      public static void main(String[] args) {
+        System.out.println(Distance("input.txt"));
+      }
 }

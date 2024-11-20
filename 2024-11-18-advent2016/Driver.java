ceb01ceec8a43aeac5c648b080e7fea8b059d481
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-11-20 11:46:46 -0500

    tried to fix q1 didn't work though

--- 2024-11-18-advent2016/Driver.java
+++ 2024-11-18-advent2016/Driver.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Driver{
+  /*
   public static int Distance(String filename){
     try {
         int dir = 0;
@@ -21,14 +22,34 @@ public class Driver{
             blocks[dir] += Integer.parseInt(dirs[i].substring(1));
           }
         }
-        System.out.println(blocks.toString());
         return blocks[0] + blocks[1]+blocks[2]+blocks[3];
+  }
+  catch(Exception e){
+    return 0;
+}
+*/
+      public static void main(String[] args) {
+        try {
+            int dir = 0;
+            int [] blocks = new int [4];
+            File file = new File("file.txt");
+            Scanner input = new Scanner(file);
+            String line = input.nextLine();
+            String [] dirs = line.split(", ");
+            for (int i = 0; i<dirs.length; i++){
+              if (dirs[i].substring(0,1).equals("R")){
+                dir++;
+                blocks[dir] += Integer.parseInt(dirs[i].substring(1));
+              }
+              else{
+                dir--;
+                blocks[dir] += Integer.parseInt(dirs[i].substring(1));
+              }
+            }
+            System.out.println( blocks[0] + blocks[1]+blocks[2]+blocks[3]);
       }
       catch(Exception e){
-      return 0;
-      }
+        System.out.println(0);
     }
-      public static void main(String[] args) {
-        System.out.println(Distance("input.txt"));
       }
 }

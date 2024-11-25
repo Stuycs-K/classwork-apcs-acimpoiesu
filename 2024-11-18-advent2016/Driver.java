8ff2aeb2cd8749f21140b99936a496439e0dfdae
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-11-24 19:52:32 -0500

    did day1 part 2

--- 2024-11-18-advent2016/Driver.java
+++ 2024-11-18-advent2016/Driver.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Driver{
@@ -7,10 +8,14 @@ public class Driver{
     try {
         int dir = 0;
         int [] blocks = new int [4];
+        ArrayList<Integer> distancesx = new ArrayList<>();
+        ArrayList<Integer> distancesy = new ArrayList<>();
         File file = new File(filename);
         Scanner input = new Scanner(file);
         String line = input.nextLine();
         String [] dirs = line.split(", ");
+        int distancex = 0;
+        int distancey = 0;
         for (int i = 0; i<dirs.length; i++){
           if (dirs[i].substring(0,1).equals("R")){
             dir = (dir + 1) % 4;
@@ -19,7 +24,13 @@ public class Driver{
             dir = (dir + 3) % 4;
           }
           blocks[dir] += Integer.parseInt(dirs[i].substring(1));
+          distancex = blocks[0] - blocks[2];
+          distancey = blocks[1] - blocks[3];
+          distancesx.add(distancex);
+          distancesy.add(distancexy);
         }
+        System.out.println(distancesx);
+        System.out.println(distancesy);
         return Math.abs(blocks[0] - blocks[2]) + Math.abs(blocks[1] - blocks[3]);
   }
   catch(Exception e){

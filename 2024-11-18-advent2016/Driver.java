702759253ab9ea6c8c23457c1c60ece0cad1ef57
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-11-24 19:32:21 -0500

    did day1 part 1

--- 2024-11-18-advent2016/Driver.java
+++ 2024-11-18-advent2016/Driver.java
@@ -3,7 +3,6 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Driver{
-  /*
   public static int Distance(String filename){
     try {
         int dir = 0;
@@ -14,42 +13,20 @@ public class Driver{
         String [] dirs = line.split(", ");
         for (int i = 0; i<dirs.length; i++){
           if (dirs[i].substring(0,1).equals("R")){
-            dir++;
-            blocks[dir] += Integer.parseInt(dirs[i].substring(1));
+            dir = (dir + 1) % 4;
           }
           else{
-            dir--;
-            blocks[dir] += Integer.parseInt(dirs[i].substring(1));
+            dir = (dir + 3) % 4;
           }
+          blocks[dir] += Integer.parseInt(dirs[i].substring(1));
         }
-        return blocks[0] + blocks[1]+blocks[2]+blocks[3];
+        return Math.abs(blocks[0] - blocks[2]) + Math.abs(blocks[1] - blocks[3]);
   }
   catch(Exception e){
     return 0;
 }
-*/
+  }
       public static void main(String[] args) {
-        try {
-            int dir = 0;
-            int [] blocks = new int [4];
-            File file = new File("file.txt");
-            Scanner input = new Scanner(file);
-            String line = input.nextLine();
-            String [] dirs = line.split(", ");
-            for (int i = 0; i<dirs.length; i++){
-              if (dirs[i].substring(0,1).equals("R")){
-                dir++;
-                blocks[dir] += Integer.parseInt(dirs[i].substring(1));
-              }
-              else{
-                dir--;
-                blocks[dir] += Integer.parseInt(dirs[i].substring(1));
-              }
-            }
-            System.out.println( blocks[0] + blocks[1]+blocks[2]+blocks[3]);
-      }
-      catch(Exception e){
-        System.out.println(0);
+      System.out.println(Distance("file.txt"));
     }
       }
-}

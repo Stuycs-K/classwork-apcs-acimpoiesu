967f23c1dea9ae1911c83cf3660cb36199762f20
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-12-06 11:36:53 -0500

    added attack and supports

--- 2024-12-06-Adventurer/Archer.java
+++ 2024-12-06-Adventurer/Archer.java
@@ -2,6 +2,7 @@ import java.util.Random;
 public class Archer extends Adventurer{
   private String SpecialName = "Arrows";
   private int SpecialAmt = 0;
+  private int SpecialMax = 999;
 
   public String getSpecialName(){
     return SpecialName;
@@ -9,4 +10,25 @@ public class Archer extends Adventurer{
   public int getSpecial(){
     return SpecialAmt;
   }
+  public void setSpecial(int n){
+    if(n < getSpecialMax()){
+    SpecialAmt = n;
+  }
+  }
+  public int getSpecialMax(){
+    return SpecialMax;
+  }
+  public String attack(Adventurer other){
+    other.applyDamage(20);
+    return "Attacked";
+  }
+  public String support(Adventurer other){
+    other.setmaxHP(100);
+    return "Healed " + other.getSpecialName();
+  }
+  public String support(){
+    this.setHP(100);
+    return "Healed self";
+  };
+  public abstract String specialAttack(Adventurer other);
 }

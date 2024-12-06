546a10155ce7d1aa552bd45f5cdfc137662a5301
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-12-06 11:27:22 -0500

    made Special and Name/amt

--- 2024-12-06-Adventurer/Archer.java
+++ 2024-12-06-Adventurer/Archer.java
@@ -1,4 +1,12 @@
 import java.util.Random;
 public class Archer extends Adventurer{
+  private String SpecialName = "Arrows";
+  private int SpecialAmt = 0;
 
+  public String getSpecialName(){
+    return SpecialName;
+  }
+  public int getSpecial(){
+    return SpecialAmt;
+  }
 }

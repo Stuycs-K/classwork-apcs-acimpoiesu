46bb3662af14d960ad9e87b1c4c85afc74e090f6
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-12-09 11:42:52 -0500

    added attack prompt

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -4,7 +4,14 @@ class Game {
     CodeWarrior p1 = new CodeWarrior ("p1", 25, "C");
     CodeWarrior p2 = new CodeWarrior ("p2", 26, "java");
     Scanner userInput = new Scanner(System.in);
-    System.out.println(p1.getName() + p1.getHP() + p1.getSpecialName());
-    System.out.println(p2.getName() + p2.getHP() + p2.getSpecialName());
+    System.out.println(p1.getName() + " "+ p1.getHP() + " " + p1.getSpecialName());
+    System.out.println(p2.getName() + " "+ p2.getHP() + " " + p2.getSpecialName());
+    while (p1.getHP()> 0 && p2.getHP()> 0){
     System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+    String input = userInput.nextLine();
+    if(input == "a" || input == "attack"){
+      p1.attack(p2);
+    }
   }
+  }
+}

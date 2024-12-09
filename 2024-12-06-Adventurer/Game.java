f2aabe28abbcb4f17f48d108a21b2c1096bdabe3
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-12-09 11:34:07 -0500

    added players

---
+++ 2024-12-06-Adventurer/Game.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+class Game {
+  public static void main(String[] args) {
+    CodeWarrior p1 = new CodeWarrior ("p1", 25, "C");
+    CodeWarrior p2 = new CodeWarrior ("p2", 26, "java");
+  }

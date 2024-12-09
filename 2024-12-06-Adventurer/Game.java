0aa64823300c9c3dea9628ce780ed370ae07cc71
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2024-12-09 11:37:40 -0500

    added printing character stats

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -3,4 +3,8 @@ class Game {
   public static void main(String[] args) {
     CodeWarrior p1 = new CodeWarrior ("p1", 25, "C");
     CodeWarrior p2 = new CodeWarrior ("p2", 26, "java");
+    Scanner userInput = new Scanner(System.in);
+    System.out.println(p1.getName() + p1.getHP() + p1.getSpecialName());
+    System.out.println(p2.getName() + p2.getHP() + p2.getSpecialName());
+    System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
   }

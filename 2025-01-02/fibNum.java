a576615d5f56f3b13e062a401c04c7bf9789228d
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2025-01-02 11:35:30 -0500

    tested method and it works

--- 2025-01-02/fibNum.java
+++ 2025-01-02/fibNum.java
@@ -1,8 +1,11 @@
 public class fibNum{
-  public int fibNums(int n){
+  public static int fibNums(int n){
     if (n < 2){
       return n;
     }
     return fibNums(n - 1) + fibNums (n - 2);
+  }
+  public static void main(String[] args) {
+    System.out.println(fibNums(3));
   }
 }

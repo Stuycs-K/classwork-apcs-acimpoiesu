6d88ca1e808b3026191c04bf6377636768cce7dd
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2025-01-03 11:45:47 -0500

    working makeWords function

--- 2025-01-02/words.java
+++ 2025-01-02/words.java
@@ -1,11 +1,12 @@
 public class words{
   public static void makeWords(int remainingLetters, String result, String alphabet){
-    if(remainingLetters < 1){
+    if(remainingLetters == 0){
       System.out.println(result);
-    }
+    }else{
     for(int i = 0; i < alphabet.length(); i++){
-      makeWords(remainingLetters - 1, result += alphabet.charAt(i), alphabet);
+      makeWords(remainingLetters - 1, result + alphabet.charAt(i), alphabet);
     }
+  }
   }
   public static void main(String[] args) {
     makeWords(2, "", "ab");

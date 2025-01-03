bb88f5f0e6ab33ebfa64390538b086fb3a4f7c37
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2025-01-03 11:40:02 -0500

    wrote code doesn't work

--- 2025-01-02/words.java
+++ 2025-01-02/words.java
@@ -1,8 +1,13 @@
 public class words{
   public static void makeWords(int remainingLetters, String result, String alphabet){
-
+    if(remainingLetters < 1){
+      System.out.println(result);
+    }
+    for(int i = 0; i < alphabet.length(); i++){
+      makeWords(remainingLetters - 1, result += alphabet.charAt(i), alphabet);
+    }
   }
   public static void main(String[] args) {
-
+    makeWords(2, "", "ab");
   }
 }

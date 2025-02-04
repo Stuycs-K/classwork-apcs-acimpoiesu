b894b28ed7336615796bf70e51793d0643b2172e
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2025-02-04 11:33:54 -0500

    added the initial for loop

--- 2025-02-04-recursion/Backtracking.java
+++ 2025-02-04-recursion/Backtracking.java
@@ -1,7 +1,11 @@
 public class Backtracking{
   public static long countNoDoubleLetterWords(int length,String word, String letters){
+    for (int i = 0; i < letters.length; i++){
+      word+= letters.charAt(i);
+      countNoDoubleLetterWords(length - 1, word, letters);
+    }
   }
   public static void main(String[] args) {
-    
+
   }
 }

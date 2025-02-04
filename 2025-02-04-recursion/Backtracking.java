dacc315f8692e2d354da5a4a9fb89f31a83ecf79
Alexandru Cimpoiesu <acimpoiesu60@stuy.edu>
2025-02-04 11:45:09 -0500

    added part of the for that checks the double letters

--- 2025-02-04-recursion/Backtracking.java
+++ 2025-02-04-recursion/Backtracking.java
@@ -1,11 +1,18 @@
 public class Backtracking{
+  static int counter = 0;
   public static long countNoDoubleLetterWords(int length,String word, String letters){
-    for (int i = 0; i < letters.length; i++){
-      word+= letters.charAt(i);
-      countNoDoubleLetterWords(length - 1, word, letters);
+    if (length == 0){
+      counter++;
     }
+    for (int i = 0; i < letters.length(); i++){
+      word+= letters.charAt(i);
+      if (letters.charAt(i) != word.charAt(word.length() -1)){
+      countNoDoubleLetterWords(length - 1, word, letters);
+      }
+    }
+    return (long) counter;
   }
   public static void main(String[] args) {
-
+    System.out.println(countNoDoubleLetterWords(2,"", "abc"));
   }
 }
